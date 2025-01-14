4212f290df107c11517a984c23365e9aacc328fb
Alessio Buccino <alejoe9187@gmail.com>
2025-01-14 11:56:38 +0100

    Fix logging

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -5,6 +5,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # GENERAL IMPORTS
 import os
+import sys
 import argparse
 import numpy as np
 from pathlib import Path
@@ -89,6 +90,28 @@ if __name__ == "__main__":
     # Use CO_CPUS env variable if available
     N_JOBS_CO = os.getenv("CO_CPUS")
     N_JOBS = int(N_JOBS_CO) if N_JOBS_CO is not None else N_JOBS
+
+    # look for subject and data_description JSON files
+    subject_id = "undefined"
+    session_name = "undefined"
+    for f in data_folder.iterdir():
+        # the file name is {recording_name}_subject.json
+        if "subject.json" in f.name:
+            with open(f, "r") as file:
+                subject_id = json.load(file)["subject_id"]
+        # the file name is {recording_name}_data_description.json
+        if "data_description.json" in f.name:
+            with open(f, "r") as file:
+                session_name = json.load(file)["name"]
+
+    if HAVE_AIND_LOG_UTILS:
+        log.setup_logging(
+            "Curate Ecephys",
+            subject_id=subject_id,
+            asset_name=session_name,
+        )
+    else:
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="%(message)s")
 
     if PARAMS_FILE is not None:
         logging.info(f"\nUsing custom parameter file: {PARAMS_FILE}")
